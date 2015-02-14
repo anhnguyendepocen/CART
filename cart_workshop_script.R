4f791c202d98e747573b5e1febcaae342fc06260
Clay Ford <joe.clayton.ford@gmail.com>
2015-02-14 12:39:18 -0500

    added pruning

--- cart_workshop_script.R
+++ cart_workshop_script.R
@@ -159,21 +159,19 @@ summary(gfit)
 98 / (98 + 98)
 
 # P(node) =1 because all observations are in the root node.
-(98 + 98) / (98 + 98)
+196 / 196
 
 # probabilities: 0.500 0.500 
-98 / (98 + 98)
-98 / (98 + 98)
+98 / 196
+98 / 196
 
 # left son=2 (76 obs) right son=3 (120 obs)
-# 76 obs sent to node 2 and 
-# 120 obs sent to node 3
-# based on split varg < 0.209 
+# 76 obs sent to node 2 and 120 obs sent to node 3 based on split varg < 0.209
 with(glaucoma, sum(varg < 0.209))
 with(glaucoma, sum(varg >= 0.209))
 
 # improve=44.01404
-# This is the reduction in impurity
+# This is the reduction in impurity.
 # It is tedious to calculate "by hand":
 
 196 * (
@@ -206,11 +204,11 @@ par(op)
 6/(70 + 6)
 
 # P(node) =0.3877551
-(70+6)/196
+76/196
 
 # probabilities: 0.921 0.079 
-70/(70+6)
-6/(70+6)
+70/76
+6/76
 
 
 # Let's look at node 3 for gfit; it's the parent node for nodes 6 and 7
@@ -228,17 +226,26 @@ par(op)
 28 / (28 + 92)
 
 # P(node) =0.6122449
-(28 + 92) / 196
+120 / 196
 
 # probabilities: 0.233 0.767
-28 / (28 + 92)
-92 / (28 + 92)
+28 / 196
+92 / 196
 
 # compare selected split to 1st runner-up:
 # mhcg < 0.1695  improve=8.738643,
 # mhci < 0.099   improve=7.836770
 
-# recall observations in node 3 have varg >= 0.209!
+
+# calculate improvement
+196 * (
+  (120/196 * (2 * 28/120 * 92/120)) - # P(node 3) * node 3 purity
+    (7/196 * (2 * 0/7 * 7/7)) -       # P(node 6) * node 6 purity
+    (113/196 * (2 * 21/113 * 92/113)) # P(node 7) * node 7 purity 
+)
+
+
+# recall observations in node 3 have varg >= 0.209
 op <- par(mfrow=c(1,2))
 stripchart(mhcg ~ Class, data=glaucoma, main="mhcg < 0.1695", 
            vertical = T, pch=1, method = "jitter",
@@ -251,11 +258,102 @@ abline(h=0.099, col="red")
 par(op)
 
 
+# Surrogate splits
 
+# These are splits that substitute for the primary split in the event an
+# observation is missing a value for the primary split.
+
+
+# Node number 1: 196 observations
+# left son=2 (76 obs) right son=3 (120 obs)
+# Primary splits:
+#   varg < 0.209   to the left,  improve=44.01404, (0 missing)
+# Surrogate splits:
+#   varn < 0.0895  to the left,  agree=0.934, adj=0.829, (0 split)
+
+# agree=0.934
+# This is proportion of observations that split in the same direction as the
+# primary.
+vargC <- with(glaucoma, cut(varg, c(-0.1,0.209,2)))
+varnC <- with(glaucoma, cut(varn, c(-0.1,0.0895,2)))
+(tab <- table(vargC,varnC))
+# The surrogate split sends 183 of the 196 obs in the same direction as the
+# primary split. Hence the agree value is 183/196 = 0.934
+(66 + 117)/196
+# or
+round(sum(diag(tab))/sum(tab),3)
+
+# adj=0.829
+
+# This is the adjusted agreement that takes into account the "majority rule". In
+# other words, if there were no surrogates we could send observations in the 
+# same direction as the majority. In this case the majority of 120 went to the 
+# right. If we substract 120 from the numerator and denominator of our original
+# "agree" calculation we get the adjusted agreement:
+round((183 - 120)/(196 - 120),3)
+
+# Variable importance
+# Form the Introduction to Rpart:
+
+# "A variable may appear in the tree many times, either as a primary or a
+# surrogate variable. An overall measure of variable importance is the sum of
+# the goodness of split measures for each split for which it was the primary
+# variable, plus goodness * (adjusted agreement) for all splits in which it was
+# a surrogate. In the printout these are scaled to sum to 100 and the rounded
+# values are shown, omitting any variable whose proportion is less than 1%."
+
+# we see that varg, vars, varn, vari, rnf and vbri were the 5 most important
+# variables.
+
+
+# summary of regression tree
 summary(ftree)
 
 
-printcp(gfit)
+# Node number 1: 1995 observations,    complexity param=0.2374845
+# mean=0.3811328, MSE=3.115265 
+# left son=2 (1962 obs) right son=3 (33 obs)
+# Primary splits:
+#   marketvalue < 89.335  to the left,  improve=0.23748450, (0 missing)
+#   sales       < 112.85  to the left,  improve=0.13006880, (0 missing)
+# 
+
+# drop records with missing profit to simplify R code a little
+forbes <- subset(forbes, !is.na(profits))
+
+# mean=0.3811328
+with(forbes, mean(profits))
+
+# MSE=3.115265 
+with(forbes, sum((profits - mean(profits))^2)/nrow(forbes))
+
+
+# improve=0.23748450
+# This is the reduction in impurity
+# It is tedious to calculate "by hand":
+SS1 <- with(forbes, sum((profits - mean(profits))^2))
+SS2 <- with(forbes[forbes$marketvalue < 89.335,], 
+            sum((profits - mean(profits))^2))
+SS3 <- with(forbes[forbes$marketvalue >= 89.335,], 
+            sum((profits - mean(profits))^2))
+1 - (SS2 + SS3)/SS1
+
+# Compare primary splits:
+#   marketvalue < 89.335  to the left,  improve=0.23748450, (0 missing)
+#   sales       < 112.85  to the left,  improve=0.13006880, (0 missing)
+
+plot(profits ~ marketvalue, data=forbes)
+abline(v=89.335, col="red")
+
+
+
+# Tree pruning ------------------------------------------------------------
+
+# The size of trees can be controled with the "control" paramter. Pass to 
+# control the function rpart.control() with arguments. For example, below we 
+# specify minsplit to be 2 and 90, respectively. This tells rpart the minimum
+# number of observations that must exist in a node in order for a split to be
+# attempted. The default minsplit setting is 20.
 
 # Tree too big; fit is too good
 gfitB <- rpart(Class ~ ., data=glaucoma, control = rpart.control(minsplit = 2))
@@ -263,9 +361,60 @@ plot(gfitB, margin=0.05, uniform=TRUE, main="A Tree too big")
 text(gfitB, cex=0.7, use.n=TRUE)
 
 # Tree too small; ignores important predictors
-gfitS <- rpart(Class ~ ., data=glaucoma, control = rpart.control(minsplit = 50))
-plot(gfitS, margin=0.05, uniform=TRUE, main="A Tree too small")
+gfitS <- rpart(Class ~ ., data=glaucoma, control = rpart.control(minsplit = 90))
+plot(gfitS, margin=0.10, uniform=TRUE, main="A Tree too small")
 text(gfitS, use.n=TRUE)
+
+# Pruning - classification tree
+
+# printcp() prints a table of optimal prunings based on a complexity parameter.
+printcp(gfit)
+
+# recall this information is also available in the summary:
+summary(gfit)
+
+# It's also printed with each node:
+# Node number 3: 120 observations,    complexity param=0.07142857
+
+# This basically says node 3 becomes "afforadable" when alpha is 0.07. When the 
+# cost-complexity is 0.07, node 3 is not pruned away but rather retained. This
+# is the highest alpha we can afford to pay to keep node 3.
+
+# plotcp() plots xerror vs cp. The dotted line is drawn 1SE above the minimum of
+# the curve.
+plotcp(gfit)
+
+# Rule of thumb: select the smallest tree within one standard error of the
+# smallest CV error.
+
+# cal also access cptable directly from rpart object
+gfit$cptable
+
+# Recall that the cptable is scaled by the number of misclassifications in a
+# model with no splits.
+gfit$cptable[,c(1,3:5)]*98
+
+# to select and plot a pruned tree:
+gfit2 <- prune(gfit, cp=0.07)
+plot(gfit2, margin=0.1)
+text(gfit2, use.n=TRUE)
+
+
+# Pruning - regression tree
+printcp(ftree)
+plotcp(ftree)
+
+# all trees appear to be within one SE of tree with smallest CV error. In this
+# case let's just prune to the smallest xerror.
+
+ftree2 <- prune(ftree, cp=0.029)
+plot(ftree2, margin=0.1)
+text(ftree2, use.n=TRUE)
+
+
+
+# Bagging and Random Forests ----------------------------------------------
+
 
 
 boxplot(varg ~ Class, data=glaucoma, main="varg")
